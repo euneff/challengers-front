72a9b66e18f5f4681cf0de869488aba86f2626c2
euneff <kt97423307@gamil.com>
2024-10-11 12:30:51 +0900

    edit review

--- src/pages/Review.js
+++ src/pages/Review.js
@@ -3,15 +3,29 @@ import React from 'react';
 function Review(){
   return(
     <div>
-  <div>
-    제목
-    <input type='text'/>
-  </div>
-  <div>
-    내용
-    <textarea></textarea>
-  </div>
-</div>
+      <div>
+        제목
+        <input type='text'/>
+      </div>
+      <div>
+        도전 이름
+        <textarea></textarea>
+      </div>
+      <div>
+        도전을 이루기 위해 어떤 노력을 하였나요?
+        <textarea></textarea>
+      </div>
+      <div>
+        보상금은 어떻게 사용하실건가요?
+        <textarea></textarea>
+      </div>
+      <button >
+        등록하기
+      </button>
+      
+    </div>
+    
+
   )
 }
 
